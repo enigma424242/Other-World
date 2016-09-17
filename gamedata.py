bb21e40aea1597fca59fce3cae50f277d3cc54de
enigma <auxiliary42@gmail.com>
2016-09-17 18:26:59 -0400

    Add files via upload

---
+++ gamedata.py
@@ -0,0 +1,361 @@
+CREATION_DATA = {
+    "Warrior": {
+        "Attributes": {
+            "Spirit": 5,
+            "Tolerance": 4,
+            "Intellect": 3,
+            "Strength": 5,
+            "Dexterity": 3
+        },
+        "Armor": {
+            "Head": "Plate Helm",
+            "Torso": "Plate Cuirass",
+            "Hands": "Plate Gauntlets",
+            "Legs": "Plate Greaves"
+        },
+        "Weapons": {
+            "Primary": "Sword",
+            "Secondary": "Shield"
+        },
+        "Inventory": {
+            "Armor": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Weapons": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Consumables": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Reusables": {
+                "Slot1": "Shrunken Head",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Key Items": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+        },
+    },
+    "Paladin": {
+        'Attributes': {
+            "Spirit": 5,
+            "Tolerance": 3,
+            "Intellect": 4,
+            "Strength": 5,
+            "Dexterity": 3
+        },
+        "Armor": {
+            "Head": "Plate Helm",
+            "Torso": "Plate Cuirass",
+            "Hands": "Plate Gauntlets",
+            "Legs": "Plate Greaves"
+        },
+        "Weapons": {
+            "Primary": "Mace",
+            "Secondary": "Shield"
+        },
+        "Inventory": {
+            "Armor": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Weapons": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Consumables": {
+                "Slot1": '',
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Reusables": {
+                "Slot1": "Dragon Scale",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Key Items": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+        },
+    },
+    "Mage": {
+        'Attributes': {
+            "Spirit": 5,
+            "Tolerance": 3,
+            "Intellect": 5,
+            "Strength": 3,
+            "Dexterity": 4
+        },
+        "Armor": {
+            "Head": "Cloth Hood",
+            "Torso": "Cloth Cloak",
+            "Hands": "Cloth Gloves",
+            "Legs": "Cloth Trousers",
+        },
+        "Weapons": {
+            "Primary": "Staff",
+            "Secondary": "Tome"
+        },
+        "Inventory": {
+            "Armor": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Weapons": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Consumables": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Reusables": {
+                "Slot1": "Monkey Paw",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Key Items": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+        },
+    },
+    "Scholar": {
+        'Attributes': {
+            "Spirit": 5,
+            "Tolerance": 4,
+            "Intellect": 5,
+            "Strength": 2,
+            "Dexterity": 4
+        },
+        "Armor": {
+            "Head": "Cloth Hood",
+            "Torso": "Cloth Cloak",
+            "Hands": "Cloth Gloves",
+            "Legs": "Cloth Trousers",
+        },
+        "Weapons": {
+            "Primary": "Talisman",
+            "Secondary": "Tome"
+        },
+        "Inventory": {
+            "Armor": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Weapons": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Consumables": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Reusables": {
+                "Slot1": "Lexicon",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Key Items": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+        },
+    },
+    "Wanderer": {
+        'Attributes': {
+            "Spirit": 4,
+            "Tolerance": 5,
+            "Intellect": 3,
+            "Strength": 3,
+            "Dexterity": 5
+        },
+        "Armor": {
+            "Head": "Leather Helm",
+            "Torso": "Leather Cuirass",
+            "Hands": "Leather Gauntlets",
+            "Legs": "Leather Greaves"
+        },
+        "Weapons": {
+            "Primary": "Bow",
+            "Secondary": "Dagger"
+        },
+        "Inventory": {
+            "Armor": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Weapons": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Consumables": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Reusables": {
+                "Slot1": "Basilisk Fang",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+            "Key Items": {
+                "Slot1": "",
+                "Slot2": "",
+                "Slot3": "",
+                "Slot4": ""
+            },
+        },
+    },
+}
+
+RESET_DATA = {
+    'Attributes': {
+        "Spirit": 0,
+        "Tolerance": 0,
+        "Intellect": 0,
+        "Strength": 0,
+        "Dexterity": 0
+    },
+    "Armor": {
+        "Head": "",
+        "Torso": "",
+        "Hands": "",
+        "Legs": ""
+    },
+    "Weapons": {
+        "Primary": "",
+        "Secondary": ""
+    },
+    "Inventory": {
+        "Armor": {
+            "Slot1": "",
+            "Slot2": "",
+            "Slot3": "",
+            "Slot4": ""
+        },
+        "Weapons": {
+            "Slot1": "",
+            "Slot2": "",
+            "Slot3": "",
+            "Slot4": ""
+        },
+        "Consumables": {
+            "Slot1": "",
+            "Slot2": "",
+            "Slot3": "",
+            "Slot4": ""
+        },
+        "Reusables": {
+            "Slot1": "",
+            "Slot2": "",
+            "Slot3": "",
+            "Slot4": ""
+        },
+        "Key Items": {
+            "Slot1": "",
+            "Slot2": "",
+            "Slot3": "",
+            "Slot4": ""
+        },
+    },
+}
+    
+
+CLASS_DESCRIPTIONS = {
+    "Warrior": "Sturdy and yielding of high strength. The warrior specializes in heavy armor and brute force melee damage.",
+    "Paladin": "They say that all paladins are direct descendants of great heroes. They yield high strength and specialize in heavy armor as well as healing.",
+    "Mage": "The mage excells in magic. With high dexterity, they are masters of illusion and muddling the minds of their foes.",
+    "Scholar": "The scholar specializes in healing and range damage. With high intellect the scholar is nothing short of brilliant.",
+    "Wanderer": "The wanderer journeys far and wide. Commoners consider them as nothing more than thieves, but the wanderer is quick and ruthless. Specializes in poison and range damage, they are not be taken lightly."
+}
+
+MYSTERY_DATA = {
+    "Shrunken Head":
+        "There's something evil about this... The skin feels like leather and the eyes and mouth have to sewn shut. It even has it's own aura. As much as I would like to get rid of it, I feel it is of great importance.",
+    "Monkey Paw":
+        "Created for black magic. Most commoners would consider this item a taboo. I can't stand the damn thing, but it almost looks as though it is reaching out for me.",
+    "Dragon Scale":
+        "Said to have once belonged to one of the eternal dragons. It radiates with a strong spiritual energy. I should hang onto it for safe keeping.",
+    "Basilisk Fang":
+        "There may very well still be some venom in this fang. It may also have some power that I am not yet aware of. It's cold, like ice running through my veins.",
+    "Lexicon":
+        "A strange puzzle with an insignia on it. I can't seem to figure it out but, maybe in time this mysterious item will show it's true power."
+}
+
+ARMOR_DATA = {
+    "Cloth Hood": 1,
+    "Cloth Cloak": 1,
+    "Cloth Gloves": 1,
+    "Cloth Trousers": 1,
+    "Leather Helm": 2,
+    "Leather Cuirass": 2,
+    "Leather Gauntlets": 2,
+    "Leather Greaves": 2,
+    "Plate Helm": 3,
+    "Plate Cuirass": 3,
+    "Plate Gauntlets": 3,
+    "Plate Greaves": 3
+}
+
+WEAPON_DATA = {
+    "Bow": 2,
+    "Dagger": 2,
+    "Mace": 3,
+    "Shield": 3,
+    "Staff": 3,
+    "Sword": 3,
+    "Talisman": 3,
+    "Tome": 1
+}
+
+MAP_DATA = {
+    "Hangmans Forest": "You awake to find yourself in a dark forest, with no recollection of how or why you are here. Evil lurks in the shadows of this dark and dismal place. If you want to get out alive, it is best that you move post haiste."
+}
