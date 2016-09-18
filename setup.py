9fea828977b506cb791596bb637167a89b698faf
enigma <auxiliary42@gmail.com>
2016-09-18 17:45:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Text Based RPG',
+    'description': 'A text-based Role Playing Game.',
     'author': 'Engima',
     'url': 'https://github.com/engima424242/RPG/blob/master/textbasedrpgtest.py',
     'download_url': 'https://github.com/enigma424242/RPG.git',
     'author_email': 'auxiliary42@gmail.com',
-    'version': '0.1',
+    'version': '0.03',
     'install_requires': ['nose'],
     'packages': ['RPG'],
     'scripts': [],
-    'name': 'textbasedrpg'
+    'name': 'Into the Shadows'
 } 
